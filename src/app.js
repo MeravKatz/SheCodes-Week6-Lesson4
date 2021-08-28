423ab24a17c93f6597c64a8bbd9c6e258f5fba4e
MeravKatz <meravkatz3@gmail.com>
2021-08-28 18:28:23 +0300

    Showing current city, temperature, humidity, wind

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+let apiKey = "861aadcbcec3a81c62e577eccbc0bb70";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showCityTemp);
+
 let currentTime = new Date();
 
 let time = document.querySelector(".time");
@@ -36,11 +40,15 @@ function searchCelsius(city) {
 }
 
 function showCityTemp(response) {
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = response.data.name;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
   let temperatureElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   celsiusTemp = Math.round(response.data.main.temp);
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 function handleSubmit(event) {
