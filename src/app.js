22a50d936278aa9c1e1b5fdff4a13d7b19f7dba4
MeravKatz <meravkatz3@gmail.com>
2021-08-30 16:16:36 +0300

    Added description

--- src/app.js
+++ src/app.js
@@ -44,11 +44,13 @@ function showCityTemp(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let iconElement = document.querySelector("#icon");
   celsiusTemp = Math.round(response.data.main.temp);
   temperatureElement.innerHTML = Math.round(celsiusTemp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.setAttribute(
